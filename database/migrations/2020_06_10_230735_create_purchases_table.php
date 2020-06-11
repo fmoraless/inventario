f99a8ad17808f60d153a342a0feffaca21109baf
Francisco Morales Sánchez <franticzero@gmail.com>
2020-06-11 13:55:02 -0400

    Generating migrations from voyager to laravel

---
+++ database/migrations/2020_06_10_230735_create_purchases_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePurchasesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('purchases', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('tipo_comprobante', 20)->nullable();
+			$table->string('serie_comprobante', 7);
+			$table->string('num_comprobante', 10)->nullable();
+			$table->dateTime('fecha_hora')->nullable();
+			$table->decimal('impuesto', 10, 0)->nullable();
+			$table->decimal('total', 10, 0)->nullable();
+			$table->string('estado', 20)->nullable();
+			$table->timestamps();
+			$table->integer('supplier_id')->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('purchases');
+	}
+
+}
